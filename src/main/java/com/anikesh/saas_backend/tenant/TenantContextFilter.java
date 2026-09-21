8497736fa6f804df540767a588900bdadb795012
Anikesh Guha <anikeshguha10@gmail.com>
2026-09-21 07:44:31 +0530

    feat: made the env file and wrote the secrets

--- src/main/java/com/anikesh/saas_backend/tenant/TenantContextFilter.java
+++ src/main/java/com/anikesh/saas_backend/tenant/TenantContextFilter.java
@@ -29,7 +29,7 @@ public class TenantContextFilter extends OncePerRequestFilter{
 
         String path=request.getRequestURI();
 
-        boolean isTenantScoped= !(path.startsWith("/auth") || (path.equals("/tenants") && request.getMethod().equals("POST"))|| path.equals("/webhooks/stripe"));
+        boolean isTenantScoped= !(path.startsWith("/api/v1/auth") || (path.equals("/api/v1/tenants") && request.getMethod().equals("POST"))|| path.equals("/api/v1/webhooks/stripe"));
 
     if (!isTenantScoped) {
             filterChain.doFilter(request, response);
